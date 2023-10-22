a1e3bce473fb0a24130ac6c7703005378a1fbfa7
NickBarkerDev <nick.barker.devops@gmail.com>
2023-10-21 22:04:31 -0700

    pointed server to root route

--- express-server/server.js
+++ express-server/server.js
@@ -4,4 +4,7 @@ const path = require("path")
 const PORT = process.env.PORT || 3500
 
 app.listen(PORT, () => console.log(`Express server is running on port ${PORT}`))
+
+// Middleware
 app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(('/', require("./routes/root.js")))
