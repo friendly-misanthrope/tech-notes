0f3cc3c90acb578c8b3adebaead19c4b9d2d42cc
NickBarkerDev <nick.barker.devops@gmail.com>
2023-10-21 23:24:59 -0700

    routed Express server to 404 page if path isn't valid

--- express-server/server.js
+++ express-server/server.js
@@ -3,8 +3,15 @@ const app = express()
 const path = require("path")
 const PORT = process.env.PORT || 3500
 
-app.listen(PORT, () => console.log(`Express server is running on port ${PORT}`))
-
 // Middleware
 app.use('/', express.static(path.join(__dirname, '/public')))
 app.use(('/', require("./routes/root.js")))
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', 'notFound.html'))
+    }
+})
+
+// Server startup
+app.listen(PORT, () => console.log(`Express server is running on port ${PORT}`))
