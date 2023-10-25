726236bf373b67088d647c745f3b476ff54cdb17
NickBarkerDev <nick.barker.devops@gmail.com>
2023-10-25 00:22:41 -0700

    added username, password, roles, and isActive fields to User model

--- express-server/models/User.model.js
+++ express-server/models/User.model.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
 const bcrypt = require('bcrypt')
 const { isEmail } = require('validator')
 
-const UserSchema = new Schema({
-
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Last name is required"],
+        minLength: [8, "Username must be at least 8 characters"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minLength: [12, "Password must be at least 12 characters"]
+    },
+    roles: [{
+        type: String,
+        default: "employee"
+    }],
+    isActive: {
+        type: Boolean,
+        default: true
+    }
 }, {timestamps: true})
 
 module.exports = mongoose.model('User', UserSchema)
