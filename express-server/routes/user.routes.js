6f85fac251097b1e5336890757b632d641b3d2d4
NickBarkerDev <nick.barker.devops@gmail.com>
2023-10-25 22:10:45 -0700

    called controller functions on their respective routes

--- express-server/routes/user.routes.js
+++ express-server/routes/user.routes.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const UserController = require('../controllers/user.controller')
 
 router.route('/')
-    .get()
-    .post()
-    .patch()
-    .delete()
+    .get(UserController.getAllUsers)
+    .post(UserController.createUser)
+    .patch(UserController.updateUser)
+    .delete(UserController.removeUser)
 
 module.exports = router
