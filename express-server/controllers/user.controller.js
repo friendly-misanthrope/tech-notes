589f2fc33c22ebbc47d4bacdefff3bcd10af066f
NickBarkerDev <nick.barker.devops@gmail.com>
2023-10-25 22:03:32 -0700

    installed asyncHandler, created empty controller to GET all users

--- express-server/controllers/user.controller.js
+++ express-server/controllers/user.controller.js
@@ -3,23 +3,12 @@ const Ticket = require('../models/Ticket.model')
 const SECRET_KEY = process.env.SECRET_KEY
 const bcrypt = require('bcrypt')
 const jsonWebToken = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
 
-module.exports.registerUser = async (req, res) => {
-    try {
-        if (potentialUser) {
-            res.status(418).json({error: {
-                errors: {
-                    username: {
-                        message: "This username already exists! Please log in."
-                    }
-                }
-            }})
-        } else {
-            const newUser = await User.create(req.body)
-            const userToken = jsonWebToken.sign({_id: newUser._id}, SECRET_KEY, {expiresIn: '2h'})
-            res.status(201).cookie('userToken', userToken, {httpOnly: true, secure: true, sameSite: 'strict', maxAge: 2 * 60 * 60 * 1000}).json(newUser)
-        }
-    } catch(err) {
-        res.status(400).json({error: err, message: "User could not be created"})
-    }
-}
+// @desc Get all users
+// @route GET /users
+// @access Private
+
+const getAllUsers = asyncHandler(async (req, res) => {
+    
+})
