062a736c6a4ab7990e3dc5ffd3ed33c9ce829414
NickBarkerDev <nick.barker.devops@gmail.com>
2023-10-25 22:08:16 -0700

    created empty controller functions for creating, updating, and removing a user

--- express-server/controllers/user.controller.js
+++ express-server/controllers/user.controller.js
@@ -5,10 +5,42 @@ const bcrypt = require('bcrypt')
 const jsonWebToken = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 
+
 // @desc Get all users
 // @route GET /users
 // @access Private
 
 const getAllUsers = asyncHandler(async (req, res) => {
+
+})
+
+
+// @desc Create a user
+// @route POST /users
+// @access Private
+const createUser = asyncHandler(async (req, res) => {
     
 })
+
+
+// @desc Update a user
+// @route PATCH /users
+// @access Private
+const updateUser = asyncHandler(async (req, res) => {
+    
+})
+
+
+// @desc Remove a user
+// @route DELETE /users
+// @access Private
+const removeUser = asyncHandler(async (req, res) => {
+    
+})
+
+module.exports = {
+    getAllUsers,
+    createUser,
+    updateUser,
+    removeUser
+}
