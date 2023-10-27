dc5e9da6811975b00ed31f6c515e2dfbd8e2a753
NickBarkerDev <nick.barker.devops@gmail.com>
2023-10-26 23:03:17 -0700

    created empty ticket controller functions for CRUD operations

--- express-server/controllers/ticket.controller.js
+++ express-server/controllers/ticket.controller.js
@@ -1,3 +1,31 @@
 const Ticket = require('../models/Ticket.model')
 const User = require('../models/User.model')
 const asyncHandler = require('express-async-handler')
+
+// @desc Get all tickets
+// @route GET /tickets
+// @access Private
+const getAllTickets = asyncHandler(async(req, res) => {
+
+})
+
+// @desc Create a ticket
+// @route POST /tickets
+// @access Private
+const createTicket = asyncHandler(async(req, res) => {
+
+})
+
+// @desc Update ticket
+// @route PATCH /tickets
+// @access Private
+const updateTicket = asyncHandler(async(req, res) => {
+
+})
+
+// @desc Delete ticket
+// @route DELETE /tickets
+// @access Private
+const deleteTicket = asyncHandler(async(req, res) => {
+
+})
