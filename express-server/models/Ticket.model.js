9e81bdd2d7e08d79b5d3ed242e78308c919ca028
NickBarkerDev <nick.barker.devops@gmail.com>
2023-10-30 20:46:44 -0700

    removed validations from ticket model

--- express-server/models/Ticket.model.js
+++ express-server/models/Ticket.model.js
@@ -4,19 +4,13 @@ const autoIncrement = require('mongoose-plugin-autoinc')
 const TicketSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        required: [true, "Ticket must be assigned to a user"],
         ref: 'User'
     },
     title: {
-        type: String,
-        required: [true, 'Title is required'],
-        maxLength: [64, 'Title must be 64 characters or less']
+        type: String
     },
     body: {
-        type: String,
-        required: [true, "Ticket body is required"],
-        minLength: [12, "Ticket body must be at least 12 characters"],
-        maxLength: [220, "Ticket body must be 220 characters or less"]
+        type: String
     },
     isComplete: {
         type: Boolean,
