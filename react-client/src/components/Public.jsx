4caa7ab8f1dd3f2adfbf6ca9987a0aafa1cb4a45
NickBarkerDev <nick.barker.devops@gmail.com>
2023-10-30 23:59:53 -0700

    Added header and top content for landing page

--- react-client/src/components/Public.jsx
+++ react-client/src/components/Public.jsx
@@ -4,7 +4,16 @@ import { Link } from 'react-router-dom'
 function Public() {
   return (
     <section className='public'>
-        
+        <header>
+            <h1>Welcome to <span className='nowrap'></span>TicTechGo!</h1>
+        </header>
+        <main className="public__addr">
+            <p>Headquartered in beautiful Seattle, Washington, tucked between the Olympic and Cascade mountains,
+                TicTechGo provides a one-stop shop for all your workflow management needs.
+                Whether your business is a small snowboard shop or a large enterprise computer repair service,
+                TicTechGo is here to help you manage employee workflow as efficiently as possible.
+            </p>
+        </main>
     </section>
   )
 }
