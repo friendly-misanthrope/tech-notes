89aff0586be7b7181f3d90c1cd4c073aa78cc430
NickBarkerDev <nick.barker.devops@gmail.com>
2023-10-31 23:19:26 -0700

    Declared hook variables, created onClick function for home button, set button only to display if user is not at the root dashboard page

--- react-client/src/components/DashFooter.jsx
+++ react-client/src/components/DashFooter.jsx
@@ -3,6 +3,25 @@ import { faHouse } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate, useLocation } from 'react-router-dom'
 
 const DashFooter = () => {
+
+    const navigate = useNavigate()
+
+    const { path } = useLocation()
+
+    const onGoHomeClicked = () => {
+        navigate('/dash')
+    }
+
+    let goHomeButton = null
+
+    if (path !== '/dash') {
+        goHomeButton = () => {
+            <button className="dash-footer__button icon-button" title="Home" onClick={onGoHomeClicked}>
+                <FontAwesomeIcon icon={faHouse} />
+            </button>
+        }
+    }
+
     const content = (
         <footer className="dash-footer">
             <p>Current user:</p>
