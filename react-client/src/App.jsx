1983badd94d57de426d3fb7d31aad192ad7bd2c2
NickBarkerDev <nick.barker.devops@gmail.com>
2023-11-01 00:03:44 -0700

    Imported Welcome component and assigned it to dash index route

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Public from './components/Public'
 import Login from './features/auth/Login'
 import DashLayout from './components/DashLayout'
+import Welcome from './features/auth/Welcome'
 
 const App = () => {
   return (
@@ -10,9 +11,11 @@ const App = () => {
       <Route path='/' element={<Layout />} />
       <Route index element={<Public />} />
       <Route path='/login' element={<Login />} />
-      <Route path='/dash' element={<DashLayout />} 
+      <Route path='/dash' element={<DashLayout />}>
         // ToDo: Components for protected routes go here
-      />
+        <Route index element={<Welcome />} />
+      </Route> 
+        
     </Routes>
   );
 }
