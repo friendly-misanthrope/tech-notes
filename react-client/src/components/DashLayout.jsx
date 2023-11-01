8e65790c870b5bfb907ab7107df1dbba90e4d9dd
NickBarkerDev <nick.barker.devops@gmail.com>
2023-10-31 23:22:40 -0700

    Imported and rendered DashFooter in DashLayout

--- react-client/src/components/DashLayout.jsx
+++ react-client/src/components/DashLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import DashHeader from './DashHeader'
+import DashFooter from './DashFooter'
 
 const DashLayout = () => {
   return (
@@ -7,6 +8,7 @@ const DashLayout = () => {
         <div className="dash-container">
             <Outlet />
         </div>
+        <DashFooter />
     </>
   )
 }
