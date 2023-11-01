91398da5dd854c36549d4b8faa2e92d025061870
NickBarkerDev <nick.barker.devops@gmail.com>
2023-10-31 23:53:31 -0700

    filled out content for Welcome component

--- react-client/src/features/auth/Welcome.jsx
+++ react-client/src/features/auth/Welcome.jsx
@@ -7,7 +7,16 @@ const Welcome = () => {
   timeStyle: 'long' })
     .format(date)
   const content = (
-
+    <section className="welcome">
+      <p>{ today }</p>
+      <h1>Welcome!</h1>
+      <p>
+        <Link to='/dash/tickets'>Tickets</Link>
+      </p>
+      <p>
+        <Link to='/dash/users'>User Settings</Link>
+      </p>
+    </section>
   )
 
   return content
