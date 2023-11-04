054216d3d84967187afc817f24b2b49a5834f8f7
NickBarkerDev <nick.barker.devops@gmail.com>
2023-11-03 23:09:29 -0700

    implemented React Helmet to allow styling/titling of individual components

--- react-client/src/components/Public.jsx
+++ react-client/src/components/Public.jsx
@@ -1,20 +1,35 @@
-import { Link } from 'react-router-dom'
-import '../styles/public.css'
+import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 function Public() {
   return (
-    <section className='public'>
-      <header>
-          <h1>Welcome to <span>TicTechGo</span></h1>
-      </header>
-      <main className="public__main">
-        <p>Headquartered in beautiful Seattle - tucked between the Olympic and Cascade mountain ranges on the shores of Puget Sound -
-          TicTechGo provides a one-stop shop for all your workflow management needs.</p>
-        <p>
-          Whether your business is a small ski/snowboard shop or a large enterprise repair service,
-          TicTechGo is here to help you efficiently delegate and manage tasks crucial to your company's continued success.
-        </p>
-        {/* <address className='public__addr'>
+    <>
+      <Helmet>
+        <link rel="stylesheet" href="./styles/public.css" />
+        <title>Welcome to TicTechGo</title>
+      </Helmet>
+      <section className="public">
+        <header>
+          <h1>
+            Welcome to <span>TicTechGo</span>
+          </h1>
+        </header>
+        <main className="public__main">
+          <h2 className="public-headline">
+            Headquartered In Beautiful Seattle, WA
+          </h2>
+          <p>
+            Tucked between the Olympic and Cascade mountain ranges on the shores
+            of Puget Sound, TicTechGo provides a one-stop shop for all your
+            workflow management needs.
+          </p>
+
+          <p>
+            Whether your business is a small ski/snowboard shop or a large
+            enterprise repair service, TicTechGo is here to help you efficiently
+            delegate and manage tasks crucial to operational productivity.
+          </p>
+          {/* <address className='public__addr'>
           <p>TicTechGo</p>
           <p>4269 Foo Street</p>
           <p>Seattle, WA 98101</p>
@@ -23,10 +38,13 @@ function Public() {
           <a href="tel:+1206-555-4269">Phone</a>
           <a href="https://www.tictechgo.com">Website</a>
         </address> */}
-      </main>
-        <footer><Link to='/login'>Login</Link></footer>
-    </section>
-  )
+        </main>
+        <footer>
+          <Link to="/login">Login</Link>
+        </footer>
+      </section>
+    </>
+  );
 }
 
-export default Public
+export default Public;
